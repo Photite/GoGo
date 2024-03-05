70af469273787972ef31c6e167f6894fef2dcdcc
Dabinger <732247442@qq.com>
2024-03-05 21:52:28 +0800

    Initial commit

---
+++ src/main/java/cn/edu/hbwe/gogo/GoGoApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.hbwe.gogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/hbwe/gogo/GoGoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.hbwe.gogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
